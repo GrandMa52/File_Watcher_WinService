c586f91a149ecbd2438c6b1e014298c2763af566
GrandMa52 <danila.mironov.1994@mail.ru>
2021-11-29 17:05:26 +0300

    Добавьте файлы проекта.

---
+++ WindowsService_techtask/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace WindowsService_techtask
+{
+    public partial class Service1 : ServiceBase
+    {
+        Logger logger;
+        public Service1()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        class Logger 
+        {
+            FileSystemWatcher watcher;
+            object obj = new object();
+            bool enabled = true;
+            public Logger() 
+            {
+                watcher = new FileSystemWatcher("C:\\Temp");
+
+            }
+        }
+    }
+}
